d5925ea54f01abc7666de76b28f5449dc9452e45
LiDingLiang-liangping <1791204370@qq.com>
2026-08-31 22:12:54 +0800

    Remove background rendering in WaypointScreen
    Removed background rendering from the WaypointScreen.

--- src/main/java/com/bettertp/WaypointScreen.java
+++ src/main/java/com/bettertp/WaypointScreen.java
@@ -81,7 +81,6 @@ public class WaypointScreen extends Screen {
 
     @Override
     public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
-        this.renderBackground(graphics, mouseX, mouseY, delta);
         super.render(graphics, mouseX, mouseY, delta);
 
         int cx = this.width / 2;
